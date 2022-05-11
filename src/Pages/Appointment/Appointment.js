37f766ee3bf5460d893fdd729d5174313ad10e52
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-11 19:19:36 +0600

    Appoinment page work in progress

---
+++ src/Pages/Appointment/Appointment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1>hello appointment</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Appointment;
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -11,10 +11,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block md:block'>
                  <img className='md:mt-[-100px] lg:mt-[-200px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white'>Make An Appointment Today</h2>
-                <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, magni omnis ducimus aspernatur, magnam at, architecto dolorem assumenda quis debitis nobis dicta provident natus. Odit corrupti numquam eos aliquam necessitatibus at quibusdam tempora eveniet incidunt! Sed ullam itaque dolore laudantium!</p>
+                <h2 className='text-3xl text-white py-5'>Make An Appointment Today</h2>
+                <p className='text-white pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, magni omnis ducimus aspernatur, magnam at, architecto dolorem assumenda quis debitis nobis dicta provident natus. Odit corrupti numquam eos aliquam necessitatibus at quibusdam tempora eveniet incidunt! Sed ullam itaque dolore laudantium!</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
         </section>
