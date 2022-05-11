a8987c469e1f7b6c3022a463e3a506da3575dbf5
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-11 23:08:56 +0600

    data fetct is done in availableAppointment

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({date, setDate}) => {
+    
     return (
         <div class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row-reverse">
--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
 
-const AvailableAppointments = () => {
+const AvailableAppointments = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    })
     return (
         <div>
-            
+            <p className='text-xl text-secondary text-center'>Available Appointments on  {format(date, "PP")}</p>
+            <div>
+                
+            </div>
         </div>
     );
 };
