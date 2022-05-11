24d3b5b9ee65dae2b7081fce670b69941c30aa44
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-12 00:38:52 +0600

    modal adding is in prgress

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,8 +1,12 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
+import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
     const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
 
     useEffect(() => {
         fetch('services.json')
@@ -12,9 +16,20 @@ const AvailableAppointments = ({ date }) => {
     return (
         <div>
             <p className='text-xl text-secondary text-center'>Available Appointments on  {format(date, "PP")}</p>
-            <div>
-                
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service
+
+                        key={service._id}
+                        service={service}
+                        setTreatment={setTreatment}
+                    ></Service>)
+                }
             </div>
+            {treatment && <BookingModal
+                setTreatment={setTreatment}
+                date={date}
+                treatment={treatment}></BookingModal>}
         </div>
     );
 };
