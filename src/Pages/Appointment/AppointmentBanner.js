9b4979914dbf78fed2a43738dc75d1cd65b58d81
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-05-12 13:08:19 +0600

    banner backgroun is added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,12 +1,17 @@
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
+import bannerBg from '../../assets/images/bg.png'
 
 
 const AppointmentBanner = ({date, setDate}) => {
     
     return (
-        <div class="hero min-h-screen">
+        <div class="hero min-h-screen sm:bg-none" style={{
+            background: `url(${bannerBg})`,
+            backgroundSize: 'cover'
+            
+        }} >
              <div class="z-0 flex items-center max-w-7xl gap-28 lg:gap-56 p-1 flex-col lg:flex-row-reverse justify-around">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
 import PrimaryButton from '../Shared/PrimaryButton';
+import bannerBg from '../../assets/images/bg.png'
+
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen">
+        <div style={{
+            background: `url(${bannerBg})`,
+            backgroundSize: 'cover'
+        }} className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} alt='#' className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
